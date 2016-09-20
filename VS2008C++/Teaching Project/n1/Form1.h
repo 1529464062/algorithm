cede6dcbcbbbaa727d8b005bb782d28d5b3e7b46
1529464062· <1529464062@qq.com>
2016-09-20 09:47:37 +0800

    update error

---
+++ VS2008C++/Teaching Project/n1/Form1.h
@@ -0,0 +1,188 @@
+#pragma once
+
+
+namespace n1 {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;	
+
+	/// <summary>
+	/// Form1 摘要
+	///
+	/// 警告: 如果更改此类的名称，则需要更改
+	///          与此类所依赖的所有 .resx 文件关联的托管资源编译器工具的
+	///          “资源文件名”属性。否则，
+	///          设计器将不能与此窗体的关联
+	///          本地化资源正确交互。
+	/// </summary>
+	public ref class Form1 : public System::Windows::Forms::Form
+	{
+	public:
+		Form1(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: 在此处添加构造函数代码
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// 清理所有正在使用的资源。
+		/// </summary>
+		~Form1()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+	private: System::Windows::Forms::Label^  label1;
+	private: System::Windows::Forms::TextBox^  textBox1;
+	private: System::Windows::Forms::TextBox^  textBox2;
+	private: System::Windows::Forms::Label^  label2;
+	private: System::Windows::Forms::TextBox^  textBox3;
+	private: System::Windows::Forms::Label^  label3;
+	private: System::Windows::Forms::TextBox^  textBox4;
+	private: System::Windows::Forms::Button^  button1;
+	private: System::Windows::Forms::ComboBox^  comboBox1;
+	protected: 
+
+	private:
+		/// <summary>
+		/// 必需的设计器变量。
+		/// </summary>
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// 设计器支持所需的方法 - 不要
+		/// 使用代码编辑器修改此方法的内容。
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox2 = (gcnew System::Windows::Forms::TextBox());
+			this->label2 = (gcnew System::Windows::Forms::Label());
+			this->textBox3 = (gcnew System::Windows::Forms::TextBox());
+			this->label3 = (gcnew System::Windows::Forms::Label());
+			this->textBox4 = (gcnew System::Windows::Forms::TextBox());
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->comboBox1 = (gcnew System::Windows::Forms::ComboBox());
+			this->SuspendLayout();
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Location = System::Drawing::Point(16, 32);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(52, 15);
+			this->label1->TabIndex = 0;
+			this->label1->Text = L"长度：";
+			// 
+			// textBox1
+			// 
+			this->textBox1->Location = System::Drawing::Point(87, 27);
+			this->textBox1->Name = L"textBox1";
+			this->textBox1->Size = System::Drawing::Size(100, 25);
+			this->textBox1->TabIndex = 1;
+			// 
+			// textBox2
+			// 
+			this->textBox2->Location = System::Drawing::Point(277, 27);
+			this->textBox2->Name = L"textBox2";
+			this->textBox2->Size = System::Drawing::Size(100, 25);
+			this->textBox2->TabIndex = 3;
+			// 
+			// label2
+			// 
+			this->label2->AutoSize = true;
+			this->label2->Location = System::Drawing::Point(206, 32);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(52, 15);
+			this->label2->TabIndex = 2;
+			this->label2->Text = L"宽度：";
+			// 
+			// textBox3
+			// 
+			this->textBox3->Location = System::Drawing::Point(467, 27);
+			this->textBox3->Name = L"textBox3";
+			this->textBox3->Size = System::Drawing::Size(100, 25);
+			this->textBox3->TabIndex = 5;
+			// 
+			// label3
+			// 
+			this->label3->AutoSize = true;
+			this->label3->Location = System::Drawing::Point(396, 32);
+			this->label3->Name = L"label3";
+			this->label3->Size = System::Drawing::Size(52, 15);
+			this->label3->TabIndex = 4;
+			this->label3->Text = L"高度：";
+			// 
+			// textBox4
+			// 
+			this->textBox4->Location = System::Drawing::Point(820, 27);
+			this->textBox4->Name = L"textBox4";
+			this->textBox4->Size = System::Drawing::Size(100, 25);
+			this->textBox4->TabIndex = 7;
+			// 
+			// button1
+			// 
+			this->button1->Location = System::Drawing::Point(726, 28);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(75, 23);
+			this->button1->TabIndex = 8;
+			this->button1->Text = L"=";
+			this->button1->UseVisualStyleBackColor = true;
+			this->button1->Click += gcnew System::EventHandler(this, &Form1::button1_Click);
+			// 
+			// comboBox1
+			// 
+			this->comboBox1->FormattingEnabled = true;
+			this->comboBox1->Items->AddRange(gcnew cli::array< System::Object^  >(4) {L"表面积", L"体积", L"外接圆体积", L"外接圆面积"});
+			this->comboBox1->Location = System::Drawing::Point(586, 28);
+			this->comboBox1->Name = L"comboBox1";
+			this->comboBox1->Size = System::Drawing::Size(121, 23);
+			this->comboBox1->TabIndex = 9;
+			// 
+			// Form1
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(8, 15);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(1610, 490);
+			this->Controls->Add(this->comboBox1);
+			this->Controls->Add(this->button1);
+			this->Controls->Add(this->textBox4);
+			this->Controls->Add(this->textBox3);
+			this->Controls->Add(this->label3);
+			this->Controls->Add(this->textBox2);
+			this->Controls->Add(this->label2);
+			this->Controls->Add(this->textBox1);
+			this->Controls->Add(this->label1);
+			this->Name = L"Form1";
+			this->Text = L"Form1";
+			this->ResumeLayout(false);
+			this->PerformLayout();
+
+		}
+#pragma endregion
+
+	private: System::Void button1_Click(System::Object^  sender, System::EventArgs^  e) {
+				 //TODO:C++数据类型的转换 方法未知
+				 //System::String a =this->textBox1->Text->Trim();
+				 //int b =atoi(a);
+				 double a,b,c,sum;
+				 a=12.2;
+				 b=15.8;
+				 c=14.9;
+				 sum = a*b*c;
+				 this->textBox4->Text = sum.ToString();
+			 }
+};
+}
+
